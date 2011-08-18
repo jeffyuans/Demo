5bdb586e7540550cdfdd2f4ce2e75e28ee2cbfd3
Marshal <jeff.yuans@gmail.com>
2011-08-17 23:51:06 -0700

    -first commit.

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
